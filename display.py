1a3ecb03be5e6802054d444580bae88045d3ff76
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-25 17:13:42 +0300

    Creating a Contact

--- display.py
+++ display.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3.6
 import pyperclip
 from user import User
-
+#To add an account
+def create_contact(account,username,password):
+    '''
+    Function to create a new account
+    '''
+    new_user = User(account,username,password)
+    return new_user
