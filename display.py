fd015610334e23afbe32aa53827d5cea418ea60f
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-25 22:37:15 +0300

    added options

--- display.py
+++ display.py
@@ -20,3 +20,14 @@ def del_contact(user):
     Function to delete a user
     '''
     user.delete_user()
+#------------------------------------------------------------
+def main():
+    print("\033[1;36;1m PASSWORD LOCKER App\n")
+    print("________________________________________________________")
+    prom = input("Hello, welcome to Password Locker there whats your name: ")
+    print("Aloha, " + prom + ", To get you started up kindly choose one of the following \n CN -> To Create an Account \n Log -> To log in \n ex -> Exit")
+
+
+
+if __name__ == '__main__':
+	main()
