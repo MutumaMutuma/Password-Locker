b19cabf66970885342362b371e0518ed510a8226
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-25 17:17:11 +0300

    debugged

--- credentials.py
+++ credentials.py
@@ -5,17 +5,3 @@ class Credential:
 	'''
 	Class to create  account credentials, generate passwords and save their information
 	'''
-# @classmethod
-# def find_user_by_account(cls,account):
-#         '''
-#         Method that takes in a account and returns a user that matches that account.
-
-#         Args:
-#             account: user account to search for
-#         Returns :
-#             Contact of person that matches the account.
-#         '''
-
-#         for user in cls.user_list:
-#             if user.account == account:
-#                 return user
